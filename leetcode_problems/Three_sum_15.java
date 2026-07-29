54867479a4c32dc5a402fdbc102203f2f1d436e4
Akil-2005 <akil8x2005@gmail.com>
2026-07-29 06:14:35 +0530

    leetcode

---
+++ leetcode_problems/Three_sum_15.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Three_sum_15{
+    public List<List<Integer>> akil(int[] arr){
+        int n=arr.length;
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(arr);
+        for(int i=0;i<n-2;i++){
+            if(i>0 && arr[i]==arr[i-1]) continue;
+            int j=i+1,k=n-1;
+            while(j<k){
+                int sum=arr[i]+arr[j]+arr[k];
+                if(sum==0){
+                    ans.add(Arrays.asList(arr[i],arr[j],arr[k]));
+                    while(j<k && arr[j]==arr[j+1]) j++;
+                    while(j<k && arr[k]==arr[k-1]) k--;
+                    j++;
+                    k--;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else j++;
+
+            }
+            
+        }
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        int[] arr={-1,0,1,2,-1,-4};
+        Three_sum_15 sol=new Three_sum_15();
+        List<List<Integer>> res=sol.akil(arr);
+        for(List<Integer> i:res){
+            for(int j:i){
+                System.out.print(j);
+            }
+            System.out.println();
+
+        }
+    }
+}
